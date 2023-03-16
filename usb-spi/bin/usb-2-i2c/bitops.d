0fcb391c75166b808f5ca49577758f80d702c394
bm16ton <bm16ton@gmail.com>
2023-03-15 23:16:15 -0400

    remove build artifacts

--- usb-spi/bin/usb-2-i2c/bitops.d
+++
@@ -1,5 +0,0 @@
-bin/usb-2-i2c/bitops.o: usb-2-i2c/bitops.c /usr/include/newlib/assert.h \
- /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
- /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
- /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
- /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h usb-2-i2c/bitops.h
--- usb-spi/module/mod2/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/media/maddocks/0e5c8f59-b86e-4423-a28c-b3830408c50a5/usb-spi-git/test5/module/mod2/modules.order := {   echo /media/maddocks/0e5c8f59-b86e-4423-a28c-b3830408c50a5/usb-spi-git/test5/module/mod2/spi_tiny_usb.o; :; } > /media/maddocks/0e5c8f59-b86e-4423-a28c-b3830408c50a5/usb-spi-git/test5/module/mod2/modules.order
